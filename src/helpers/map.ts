bb7f6e5e8d54950d14f4e69c12e28e735b4626f1
valek-web <ziryanovvalentin2@gmail.com>
2024-06-07 15:06:56 +0300

    add create geozone

--- src/helpers/map.ts
+++ src/helpers/map.ts
@@ -48,3 +48,55 @@ export const calculateCirclePoints = (center, radius, numberOfPoints) => {
   }
   return points
 }
+
+export const douglasPeucker = (
+  points: { lat: number; lng: number }[],
+  tolerance: number,
+): { lat: number; lng: number }[] => {
+  if (points.length <= 2) {
+    return [points[0]]
+  }
+
+  let maxDistance = 0
+  let index = 0
+  const end = points.length - 1
+
+  for (let i = 1; i < end; i++) {
+    const distance = perpendicularDistance(points[i], points[0], points[end])
+    if (distance > maxDistance) {
+      index = i
+      maxDistance = distance
+    }
+  }
+
+  if (maxDistance > tolerance) {
+    const firstPart = douglasPeucker(points.slice(0, index + 1), tolerance)
+    const secondPart = douglasPeucker(points.slice(index), tolerance)
+
+    if (
+      firstPart[firstPart.length - 1].lat === secondPart[0].lat &&
+      firstPart[firstPart.length - 1].lng === secondPart[0].lng
+    ) {
+      secondPart.shift()
+    }
+
+    return firstPart.concat(secondPart)
+  } else {
+    return [points[0], points[end]]
+  }
+}
+
+const perpendicularDistance = (
+  point: { lat: number; lng: number },
+  lineStart: { lat: number; lng: number },
+  lineEnd: { lat: number; lng: number },
+): number => {
+  const { lat: x1, lng: y1 } = lineStart
+  const { lat: x2, lng: y2 } = lineEnd
+  const { lat: x, lng: y } = point
+
+  const numerator = Math.abs((x2 - x1) * (y1 - y) - (x1 - x) * (y2 - y1))
+  const denominator = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+
+  return numerator / denominator
+}
